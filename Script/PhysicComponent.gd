dcbd3650069857d0e26a4bd084d0f1e1eb7aefa9
Kesinew <guhilkaw@gmail.com>
2024-10-08 13:36:12 +0700

    PhysicComponentVersion2
    Kuy

--- Script/PhysicComponent.gd
+++ Script/PhysicComponent.gd
@@ -9,81 +9,68 @@ var velocity = Vector2.ZERO
 var drift_factor = 0.2
 
 # Signals to handle input from player script
-signal throttle_pressed
-signal brake_pressed
-signal turn_left
-signal turn_right
+signal go_up
+signal go_down
+signal go_left
+signal go_right
 signal no_input
 
 # Flags to control the state
-var is_throttling = false
-var is_braking = false
-var is_turning_left = false
-var is_turning_right = false
+var is_go_up = false
+var is_go_down = false
+var is_go_left = false
+var is_go_right = false
 
 func _ready():
 	# Connect the signals from the player script
-	connect("throttle_pressed", self, "_on_throttle_pressed")
-	connect("brake_pressed", self, "_on_brake_pressed")
-	connect("turn_left", self, "_on_turn_left")
-	connect("turn_right", self, "_on_turn_right")
+	connect("go_up", self, "_on_throttle_pressed")
+	connect("go_down", self, "_on_brake_pressed")
+	connect("go_left", self, "_on_turn_left")
+	connect("go_right", self, "_on_turn_right")
 	connect("no_input", self, "_on_no_input")
 
 func _physics_process(_delta):
 	# Steering and throttle control based on flags
-	var input_vector = Vector2.ZERO
-
-	if is_throttling:
+	var input_vector = Vector2(0 , 0)
+	#Handling Singal#
+	if is_go_up:
 		input_vector.y -= 1
-		print(velocity)
-	elif is_braking:
+	elif is_go_down:
 		input_vector.y += 1
 
-	if is_turning_left:
-		rotation -= rotation_speed * _delta
-	elif is_turning_right:
-		rotation += rotation_speed * _delta
+	if is_go_left:
+		input_vector.x -= 1
+	elif is_go_right:
+		input_vector.x += 1
 
 	# Apply acceleration and friction
-	var direction = Vector2(cos(rotation), sin(rotation))
-	if input_vector.y != 0:
-		velocity += direction * input_vector.y * acceleration * _delta
-	else:
-		velocity = velocity.linear_interpolate(Vector2.ZERO, friction * _delta)
+	if input_vector.length() > 0:
+		velocity += input_vector.normalized() * acceleration * _delta
 
 	# Clamp the velocity to max speed
 	velocity = velocity.limit_length(max_speed)
-
-	# Apply drift and movement
-	var drift_vector = velocity.rotated(rotation).normalized()
-	velocity = velocity.linear_interpolate(drift_vector * velocity.length(), drift_factor)
-
-	# Move the player
 	velocity = move_and_slide(velocity)
 
-# Signal handlers
-#func _on_throttle_pressed():
-
+#Signal Funaction
 func _on_throttle_pressed():
-	is_throttling = true
-	is_braking = false
+	is_go_up = true
+	is_go_down= false
 func _on_brake_pressed(): 
-	is_braking = true
-	is_throttling = false
+	is_go_down = true
+	is_go_up = false
 
 func _on_turn_left():
-	is_turning_left = true
-	is_turning_right = false
+	is_go_left = true
+	is_go_right= false
 
 func _on_turn_right():
-	is_turning_right = true
-	is_turning_left = false
+	is_go_right = true
+	is_go_left = false
 
 func _on_no_input():
-	is_turning_left = false
-	is_turning_right = false
-	is_throttling = false
-	is_braking= false
-
+	is_go_up = false
+	is_go_down= false
+	is_go_right = false
+	is_go_left = false
 
 
--- Script/PlayerInputHandler.gd
+++ Script/PlayerInputHandler.gd
@@ -10,14 +10,15 @@ func _ready():
 	else:
 		print("PhysicsComponent loaded successfully: ", physics_component)
 func _process(_delta):
+		#Handle InputData#
 	if Input.is_action_pressed("ui_up"):
-		p_node.emit_signal("throttle_pressed")
+		p_node.emit_signal("go_up")
 	elif Input.is_action_pressed("ui_down"):
-		p_node.emit_signal("brake_pressed")
+		p_node.emit_signal("go_down")
 	if Input.is_action_pressed("ui_left"):
-		p_node.emit_signal("turn_left")
+		p_node.emit_signal("go_left")
 	elif Input.is_action_pressed("ui_right"):
-		p_node.emit_signal("turn_right")
+		p_node.emit_signal("go_right")
 	else:
 		p_node.emit_signal("no_input")
 
