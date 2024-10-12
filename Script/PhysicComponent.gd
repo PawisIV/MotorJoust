eebbee4dae962775013caf5ac0ec0644bca935a2
Kesinew <guhilkaw@gmail.com>
2024-10-12 20:21:25 +0700

    Physic_Component_V3
    Make Physic Com more like physic

--- Script/PhysicComponent.gd
+++ Script/PhysicComponent.gd
@@ -1,49 +1,32 @@
 extends KinematicBody2D
 
 # Variables for movement
-var acceleration = 200.0
-var max_speed = 800.0
+var acceleration = 0.0
+var max_speed = 0.0
 var friction = 0
-var rotation_speed = 0.1
 var velocity = Vector2.ZERO
-var drift_factor = 0.2
+var input_vector = Vector2.ZERO
 
 # Signals to handle input from player script
-signal go_up
-signal go_down
-signal go_left
-signal go_right
+signal force_up
+signal force_down
+signal force_left
+signal force_right
 signal no_input
+signal set_var
 
-# Flags to control the state
-var is_go_up = false
-var is_go_down = false
-var is_go_left = false
-var is_go_right = false
 
 func _ready():
 	# Connect the signals from the player script
-	connect("go_up", self, "_on_throttle_pressed")
-	connect("go_down", self, "_on_brake_pressed")
-	connect("go_left", self, "_on_turn_left")
-	connect("go_right", self, "_on_turn_right")
+	connect("force_up", self, "_on_up")
+	connect("force_down", self, "_on_down")
+	connect("force_left", self, "_on_left")
+	connect("force_right", self, "_on_right")
 	connect("no_input", self, "_on_no_input")
+	connect("set_var", self, "on_set_variable")
 
 func _physics_process(_delta):
-	# Steering and throttle control based on flags
-	var input_vector = Vector2(0 , 0)
-	#Handling Singal#
-	if is_go_up:
-		input_vector.y -= 1
-	elif is_go_down:
-		input_vector.y += 1
-
-	if is_go_left:
-		input_vector.x -= 1
-	elif is_go_right:
-		input_vector.x += 1
-
-	# Apply acceleration and friction
+	# Apply acceleration and update velocity based on input
 	if input_vector.length() > 0:
 		velocity += input_vector.normalized() * acceleration * _delta
 
@@ -51,26 +34,30 @@ func _physics_process(_delta):
 	velocity = velocity.limit_length(max_speed)
 	velocity = move_and_slide(velocity)
 
-#Signal Funaction
-func _on_throttle_pressed():
-	is_go_up = true
-	is_go_down= false
-func _on_brake_pressed(): 
-	is_go_down = true
-	is_go_up = false
+# Signal Functions
+func _on_up(arg1):
+	# Only modify the y-axis without resetting the x-axis
+	input_vector.y = -arg1
 
-func _on_turn_left():
-	is_go_left = true
-	is_go_right= false
 
-func _on_turn_right():
-	is_go_right = true
-	is_go_left = false
+func _on_down(arg1):
+	# Only modify the y-axis without resetting the x-axis
+	input_vector.y = arg1
+
+func _on_left(arg1):
+	# Only modify the x-axis without resetting the y-axis
+	input_vector.x = -arg1
+
+
+func _on_right(arg1):
+	# Only modify the x-axis without resetting the y-axis
+	input_vector.x = arg1
 
 func _on_no_input():
-	is_go_up = false
-	is_go_down= false
-	is_go_right = false
-	is_go_left = false
+	# Reset input vector to stop movement
+	input_vector = Vector2.ZERO
 
-
+func on_set_variable(arg1, arg2, arg3):
+	acceleration = arg1
+	max_speed = arg2
+	friction = arg3
--- Script/PlayerComponent.gd
+++ Script/PlayerComponent.gd
@@ -1,27 +1,37 @@
 extends Node2D
-
-
+export var acceleration = 200.0
+export var max_speed = 800.0
+export var friction = 0
 # Reference to the physics component
 onready var physics_component = $PhysicComponent
 onready var p_node = get_node("PhysicComponent")
+
 func _ready():
 	if physics_component == null:
 		print("PhysicsComponent is null, check the node path!")
 	else:
 		print("PhysicsComponent loaded successfully: ", physics_component)
+	p_node.emit_signal("set_var",acceleration,max_speed,friction)
+
 func _process(_delta):
-		#Handle InputData#
 	if Input.is_action_pressed("ui_up"):
-		p_node.emit_signal("go_up")
+		p_node.emit_signal("force_up", 1)
 	elif Input.is_action_pressed("ui_down"):
-		p_node.emit_signal("go_down")
-	if Input.is_action_pressed("ui_left"):
-		p_node.emit_signal("go_left")
-	elif Input.is_action_pressed("ui_right"):
-		p_node.emit_signal("go_right")
+		p_node.emit_signal("force_down", 1)
 	else:
+	# If neither up nor down is pressed, we send 0
+		p_node.emit_signal("force_up", 0)  # Neutral value for vertical movement
+		p_node.emit_signal("force_down", 0)  # Neutral value for vertical movement
+
+	if Input.is_action_pressed("ui_left"):
+		p_node.emit_signal("force_left", 1)
+	elif Input.is_action_pressed("ui_right"):
+		p_node.emit_signal("force_right", 1)
+	else:
+	# If neither left nor right is pressed, we send 0
+		p_node.emit_signal("force_left", 0)  # Neutral value for horizontal movement
+		p_node.emit_signal("force_right", 0)  # Neutral value for horizontal movement
+
+# Emit no input signal only when no direction is pressed
+	if not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
 		p_node.emit_signal("no_input")
-
-
-
-
