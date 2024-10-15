4f651d04841e80fc57732b6ebcfa9b0e128f4a59
Kesinew <guhilkaw@gmail.com>
2024-10-15 15:59:12 +0700

    Add Animatinon placeho

--- Script/PlayerComponent.gd
+++ Script/PlayerComponent.gd
@@ -5,7 +5,8 @@ export var friction = 0
 # Reference to the physics component
 onready var physics_component = $PhysicComponent
 onready var p_node = get_node("PhysicComponent")
-
+onready var a_node = get_node("PhysicComponent/PlayerAnimator")
+signal play_animation(anim_name,flip)
 func _ready():
 	if physics_component == null:
 		print("PhysicsComponent is null, check the node path!")
@@ -13,24 +14,39 @@ func _ready():
 		print("PhysicsComponent loaded successfully: ", physics_component)
 	p_node.emit_signal("set_var",acceleration,max_speed,friction)
 
+
 func _process(_delta):
 	if Input.is_action_pressed("ui_up"):
 		p_node.emit_signal("force_up", 1)
+		if p_node.velocity.y > 0 :
+			emit_signal("play_animation","PlayerUpRight",true) 
+		elif p_node.velocity.y < 0 :
+			emit_signal("play_animation","PlayerUpRight",false) 
+		elif p_node.velocity.y == 0 :
+			emit_signal("play_animation","PlayerUp",false) 
+
 	elif Input.is_action_pressed("ui_down"):
 		p_node.emit_signal("force_down", 1)
+		if p_node.velocity.y > 0 :
+			emit_signal("play_animation","PlayerDownRight",true) 
+		elif p_node.velocity.y < 0 :
+			emit_signal("play_animation","PlayerDownRight",false) 
+		elif p_node.velocity.y == 0 :
+			emit_signal("play_animation","PlayerDown",false) 
 	else:
-	# If neither up nor down is pressed, we send 0
 		p_node.emit_signal("force_up", 0)  # Neutral value for vertical movement
 		p_node.emit_signal("force_down", 0)  # Neutral value for vertical movement
 
 	if Input.is_action_pressed("ui_left"):
 		p_node.emit_signal("force_left", 1)
+		emit_signal("play_animation","PlayerRight",true) #testanim
 	elif Input.is_action_pressed("ui_right"):
 		p_node.emit_signal("force_right", 1)
+		emit_signal("play_animation","PlayerRight",false) #testanim
 	else:
-	# If neither left nor right is pressed, we send 0
-		p_node.emit_signal("force_left", 0)  # Neutral value for horizontal movement
-		p_node.emit_signal("force_right", 0)  # Neutral value for horizontal movement
+		p_node.emit_signal("force_right", 0)
+		p_node.emit_signal("force_left", 0)
+
 
 # Emit no input signal only when no direction is pressed
 	if not Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
